1c545f9f260862ee92d822d8d685fbe85ade9d06
mirela777 <70278105+mirela777@users.noreply.github.com>
2021-08-03 11:03:04 +0200

    Update hangman.js

--- hangman.js
+++ hangman.js
@@ -1,73 +1,74 @@
-var words = ['cat', 'dog', 'orange',' apple','power', 'internet', 'software'];
+var words = ['cat', 'dog', 'orange',' apple','power', 'internet', 'software','country','decision','education','error'];
 var answerArray = []; 
 var totalGuesses;
+var counter = 0;
 
 function getRandomWord() {                                 
-      var randomWord = words[Math.floor(Math.random()* words.length)];
-      return randomWord;                      
+     var randomWord = words[Math.floor(Math.random()* words.length)];
+     return randomWord;                      
 }
 
-function generateAlphabet(){
-      var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-      for (var i =0; i < alphabet.length; i++) {
-          var btn = document.createElement("BUTTON");
-          btn.setAttribute("class", "btn btn-primary");
-          btn.style.margin = "10px";
-          btn.innerHTML = alphabet[i];
-          btn.id = alphabet[i];
-          document.getElementById("letters").appendChild(btn);
-          btn.addEventListener("click", function(){
-          document.getElementById(this.id).disabled = true;
-          checkLetter(this.id);
-             });  
-        }}
+function generateLetters(){
+     var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+     for (var i = 0; i < alphabet.length; i++) {
+         var btn = document.createElement("BUTTON");
+         btn.setAttribute("class", "btn btn-primary");
+         btn.style.margin = "10px";
+         btn.innerHTML = alphabet[i];
+         btn.id = alphabet[i];
+         document.getElementById("letters").appendChild(btn);
+         btn.addEventListener("click", function(){
+         document.getElementById(this.id).disabled = true;
+         checkLetter(this.id);
+         });  
+      }}
         
-function generateAnswer(word){ 
-      for (var i = 0; i < word.length; i++) {
-          var letters = document.createElement("SPAN");
-          letters.style.margin = "6px";
-          letters.innerText = " _";
-          answerArray[i] = word[i];
-          document.getElementById("generateAnswer").appendChild(letters);
-          letters.addEventListener("change",function(){
-          checkLetter(this.id);
-                })
-            }}
+function guessedLetters(word){ 
+     for (var i = 0; i < word.length; i++) {
+         var letters = document.createElement("SPAN");
+         letters.style.margin = "6px";
+         letters.innerText = " _";
+         answerArray[i] = word[i];
+         document.getElementById( "guessedLetters").appendChild(letters);
+         letters.addEventListener("change",function(){
+         checkLetter(this.id);
+            })
+      }}
 
 function startGame(){
-      generateAlphabet();
-      var word = getRandomWord();
-       generateAnswer(word); 
-       totalGuesses = word.length + 1;
-       document.getElementById("guesses").innerText = totalGuesses;  
+     generateLetters();
+     var word = getRandomWord();
+     guessedLetters(word); 
+     totalGuesses = word.length + 1;
+     document.getElementById("guesses").innerText = totalGuesses;  
 }
-var counter = 0;
+
 function checkLetter(letter){
-         var contains = 0; 
-         for(var i = 0; i < answerArray.length; i++){
-            if(letter == answerArray[i]){
+     var contains = 0; 
+     for (var i = 0; i < answerArray.length; i++){
+         if(letter == answerArray[i]){
             contains = 1;
             counter ++;
-            document.getElementById("generateAnswer").children[i].innerText = letter;
+            document.getElementById( "guessedLetters").children[i].innerText = letter;
+         }
       }
-   }
          if(contains == 0){
              document.getElementById("guesses").innerText = totalGuesses - 1;
              totalGuesses -= 1;
-            }
+         }
          if(totalGuesses == 0){
-            document.getElementById("message").innerHTML = "You lose! " +" The word was " + answerArray.join("");
+            document.getElementById("message").innerHTML = "You lose! " + " The word was " + answerArray.join("");
             setTimeout(function() {
                location.reload();
              }, 2000);    
-   }
+         }
          if(counter == answerArray.length){
             document.getElementById("message").innerHTML = "You won!";
             setTimeout(function() {
                location.reload();
              }, 2000);  
-   }
+         }
 }
 function restartGame() {
-        location.reload();
+     location.reload();
   }
